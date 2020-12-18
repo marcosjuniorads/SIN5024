9e8e369c20fa7bf39b3d74e251c1dc14a41502ce
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N.lan>
2020-12-17 21:09:23 -0300

    teste git

--- convex recoloration/py/functions_callback.py
+++ convex recoloration/py/functions_callback.py
@@ -4,6 +4,7 @@ from functions_model import *
 from functions_files import *
 from gurobipy.gurobipy import LinExpr, quicksum
 
+#testegit
 
 def color_separation(model):
     variaveis = model.getVars()
